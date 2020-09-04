15062aa72a86d053712aa01538ced2385229e540
ramya <ramya.rolly@gmail.com>
2020-09-04 14:41:38 -0400

    Initialize project using Create React App

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const  firebaseConfig = {
+    apiKey: "AIzaSyAjTXuWT6RDUZ0facy68iUNNbCEPcx994A",
+    authDomain: "watsapp-mern.firebaseapp.com",
+    databaseURL: "https://watsapp-mern.firebaseio.com",
+    projectId: "watsapp-mern",
+    storageBucket: "watsapp-mern.appspot.com",
+    messagingSenderId: "476049061121",
+    appId: "1:476049061121:web:61704c88ca4dd0f6ec9700",
+    measurementId: "G-PMZM63BJEP"
+  };
